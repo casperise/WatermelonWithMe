03cfd05f22669a674be034e129a8d91c6eb25dcc
Jozef Prídavok <jozef@pridavok.sk>
2013-03-09 21:42:54 +0100

    The first version.

---
+++ native/jsb.js
@@ -0,0 +1,10 @@
+//
+// Javascript Bindigns helper file
+//
+
+// DO NOT ALTER THE ORDER
+require('native/jsb_cocos2d.js');
+require('native/jsb_chipmunk.js');
+require('native/jsb_opengl.js');
+require('native/jsb_cocosbuilder.js');
+require('native/jsb_sys.js');
---
+++ native/main.js
@@ -0,0 +1,16 @@
+/*
+ Watermelon With Me
+ Boot file for JSB
+ */
+
+// JS Bindings constants
+require("native/jsb.js");
+
+// resource file
+require('native/resources-jsb.js');
+
+// Level file
+require('src/levels.js');
+
+// game file
+require('src/watermelon_with_me.js');
